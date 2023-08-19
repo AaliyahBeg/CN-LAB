4e087c6a57e090d7f580bbf0158c7010bff0fe74
Aaliyah Beg <aaliyahbeg123@gmail.com>
2023-08-19 13:15:37 +0530

    Adding Q4

---
+++ Q4_Server_Client/server.js
@@ -0,0 +1,30 @@
+const { WebSocketServer } = require("ws");
+const wss = new WebSocketServer({ port: 5000 });
+const readline = require("readline");
+
+console.log("Listening on port 5000");
+
+//singleton readline
+let rl;
+
+/**
+ * @description
+ * 1. wss.on('connection') is triggered when the client is connected to the server
+ * 2. ws.on('message') is triggered when the client sends a message to the server
+ * 3. ws.on('close') is triggered when the client is disconnected from the server
+ */
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    console.log("\nClient: " + message);
+    //pass the message to getdatatype() function which will later call getdatatype function
+    console.log("Message Recieved Successfully!");
+  });
+
+
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
